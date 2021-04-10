5434d28a569f6e5d81f967867dd6c6f0959d06dc
Deniz <deniz.yavascan@gmail.com>
2021-04-10 17:38:33 +0300

    v1

---
+++ src/GitTest/C3.java
@@ -0,0 +1,14 @@
+package GitTest;
+
+public class C3 {
+
+	public static void main(String[] args) {
+		
+		 GitTestClass xii=new GitTestClass();
+		 System.out.println(xii.i);
+		 xii.i=5;
+		 System.out.println(xii.i);
+		
+	}
+
+}
--- src/GitTest/GitTestClass.java
+++ src/GitTest/GitTestClass.java
@@ -1,9 +1,50 @@
 package GitTest;
 
 public class GitTestClass {
-
+	 int i=9;
+    static char chr;
+    static String sr;
+    static int sttc;
+    
 	public static void main(String[] args) {
        System.out.println("Gİt den mesaj");
+      int xx=0;
+      
+      GitTestClass ii=new GitTestClass();
+      
+        
+      System.out.println(sttc);
+      System.out.println(ii.i);
+      System.out.println(chr);
+      System.out.println(sr);
+      cls();
+      
+   
+		
+    	  ii.i=8;
+    	  System.out.println(ii.i);
+
+        
+      
+	}
+
+	public static void cls() {
+		
+	 GitTestClass xi=new GitTestClass();
+		   System.out.println(xi.i);
+		   
+		   
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
